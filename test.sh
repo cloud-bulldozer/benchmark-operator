42f1f12d973967ebcddde32a3c7e7d176b3a5215
agopi <agopi@redhat.com>
2019-03-21 03:58:48 -0400

    Updated CI testing structure
    1. Divided test into functional and integration sort tests
    2. Using kubectl wait mechanism instead of using sleep

--- test.sh
+++ test.sh
@@ -1,60 +1,19 @@
 #!/usr/bin/env bash
 set -xeo pipefail
-function finish {
-  echo "Exiting after cleanup"
-  kubectl delete -f deploy/operator.yaml
-  kubectl delete -f deploy/crds/bench_v1alpha1_bench_crd.yaml
-  kubectl delete -f deploy/service_account.yaml
-  kubectl delete -f deploy/role_binding.yaml
-  kubectl delete -f deploy/role.yaml
-}
 
-function wait_clean {
-  for i in {1..30}; do
-    if [ `kubectl get pods|wc -l` -ge 2 ]; then
-      sleep 5
-    else
-      break
-    fi
-  done
-}
+source tests/common.sh
 
-trap finish EXIT
+trap cleanup_resources EXIT
 
-kubectl apply -f deploy/role.yaml
-kubectl apply -f deploy/role_binding.yaml
-kubectl apply -f deploy/service_account.yaml
-kubectl apply -f deploy/crds/bench_v1alpha1_bench_crd.yaml
-
-operator-sdk build quay.io/rht_perf_ci/benchmark-operator
-
-docker push quay.io/rht_perf_ci/benchmark-operator
-
-sed -i 's|          image: *|          image: quay.io/rht_perf_ci/benchmark-operator:latest # |' deploy/operator.yaml
-
+operator_requirements
+update_operator_image
 #
-# Iterate through workloads individually
+# Run functional test for workloads
 #
 # Test UPerf
-kubectl apply -f deploy/operator.yaml
 /bin/bash tests/test_uperf.sh
-kubectl delete -f tests/test_crs/valid_uperf.yaml
-kubectl delete -f deploy/operator.yaml
-
 wait_clean
-
 #
 # Test FIO
-kubectl apply -f deploy/operator.yaml
 /bin/bash tests/test_fio.sh
-kubectl delete -f tests/test_crs/valid_fio.yaml
-kubectl delete -f deploy/operator.yaml
-
 wait_clean
-
-#
-# Test all workloads
-#
-kubectl apply -f deploy/operator.yaml
-/bin/bash tests/test_uperf.sh
-/bin/bash tests/test_fio.sh
