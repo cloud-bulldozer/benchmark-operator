cc093120fd5c127f153178f494cdb11c26ae18dd
agopi <agopi@redhat.com>
2019-03-21 03:58:48 -0400

    Updated CI testing structure
    1. Divided test into functional and integration sort tests
    2. Using kubectl wait mechanism instead of using sleep

--- tests/test_fio.sh
+++ tests/test_fio.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 set -xeo pipefail
 
-# Instead of applying the cr, we should create different crs and
-kubectl apply -f tests/test_crs/valid_fio.yaml
-sleep 30
-fio_client_pod=$(kubectl get pods -l app=fio-bench-server -o name | cut -d/ -f2)
-sleep 150
-# ensuring the run has actually happened
-kubectl logs "$fio_client_pod" | grep "Run status"
+source tests/common.sh
+
+function finish {
+  echo "Cleaning up Fio"
+  kubectl delete -f tests/test_crs/valid_fio.yaml
+  delete_operator
+}
+
+trap finish EXIT
+
+function functional_test_uperf {
+  apply_operator
+  kubectl apply -f tests/test_crs/valid_fio.yaml
+  sleep 30
+  fio_pod=$(kubectl get pods -l app=fio-bench-server -o name | cut -d/ -f2)
+  kubectl wait --for=condition=Initialized "pods/$fio_pod" --timeout=200s
+  kubectl wait --for=condition=Ready "pods/$fio_pod" --timeout=200s
+  sleep 30
+  # ensuring the run has actually happened
+  kubectl logs "$fio_pod" | grep "Run status"
+}
+
+functional_test_uperf
--- tests/test_uperf.sh
+++ tests/test_uperf.sh
@@ -3,15 +3,28 @@ set -xeo pipefail
 
 source tests/common.sh
 
-# Instead of applying the cr, we should create different crs and
-kubectl apply -f tests/test_crs/valid_uperf.yaml
-check_status 2
-uperf_client_pod=$(kubectl get pods -l app=uperf-bench-client -o name | cut -d/ -f2)
+function finish {
+  echo "Cleaning up Uperf"
+  kubectl delete -f tests/test_crs/valid_uperf.yaml
+  delete_operator
+}
 
-check_log $uperf_client_pod "Success"
-#timeout 500 kubectl logs -f $uperf_client_pod
+trap finish EXIT
 
-kubectl get pods -l name=benchmark-operator -o name | cut -d/ -f2 | xargs -I{} kubectl exec {} -- cat /tmp/current_run
+function functional_test_uperf {
+  apply_operator
+  kubectl apply -f tests/test_crs/valid_uperf.yaml
+  check_pods 2
+  uperf_client_pod=$(kubectl get pods -l app=uperf-bench-client -o name | cut -d/ -f2)
 
-# ensuring that uperf actually ran and we can access metrics
-kubectl logs "$uperf_client_pod" | grep Success
+  kubectl wait --for=condition=Initialized "pods/$uperf_client_pod" --timeout=200s
+  kubectl wait --for=condition=Ready "pods/$uperf_client_pod" --timeout=45s
+  #check_log $uperf_client_pod "Success"
+  # This is for the operator playbook to finish running
+  sleep 30
+  kubectl get pods -l name=benchmark-operator -o name | cut -d/ -f2 | xargs -I{} kubectl exec {} -- cat /tmp/current_run
+
+  # ensuring that uperf actually ran and we can access metrics
+  kubectl logs "$uperf_client_pod" | grep Success
+}
+functional_test_uperf
