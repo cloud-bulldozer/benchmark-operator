b93fbdc5d39bdba51a417acad61d97ef53eb8d7f
Aakarsh Gopi <aakarsh.g2012@gmail.com>
2019-03-22 00:20:48 -0400

    Added wait condition complete for sysbench job
    Waiting for pod to hit ready state was a shot in dark for sysbench and a more reliable manner was to wait for the job to complete

--- tests/test_sysbench.sh
+++ tests/test_sysbench.sh
@@ -14,12 +14,12 @@ trap finish EXIT
 function functional_test_sysbench {
   apply_operator
   kubectl apply -f tests/test_crs/valid_sysbench.yaml
-  check_pods 1
+  sleep 15
   sysbench_pod=$(kubectl get pods -l app=sysbench -o name | cut -d/ -f2)
   kubectl wait --for=condition=Initialized "pods/$sysbench_pod" --timeout=200s
   # Higher timeout as it takes longer
-  kubectl wait --for=condition=Ready "pods/$sysbench_pod" --timeout=400s
-  sleep 30
+  kubectl wait --for=condition=complete -l app=sysbench jobs
+  # sleep isn't needed as the sysbench is kind: job so once it's complete we can access logs
   # ensuring the run has actually happened
   kubectl logs "$sysbench_pod" | grep "execution time"
 }
