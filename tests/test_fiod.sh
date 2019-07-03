f1ff7834bd354fa0f2618179b75aeee7a12ad21e
aakarshg <aakarsh.g2012@gmail.com>
2019-07-03 00:07:58 -0400

    running multiple samples with fio-d

--- tests/test_fiod.sh
+++ tests/test_fiod.sh
@@ -15,10 +15,10 @@ function functional_test_fio {
   #figlet $(basename $0)
   apply_operator
   kubectl apply -f tests/test_crs/valid_fiod.yaml
-  pod_count 'app=fio-benchmark' 2 300
-  fio_pod=$(get_pod 'app=fiod-client' 300)
+  pod_count 'app=fio-benchmark-0' 2 300
+  fio_pod=$(get_pod 'app=fiod-client-0' 300)
   kubectl wait --for=condition=Initialized "pods/$fio_pod" --namespace ripsaw --timeout=200s
-  kubectl wait --for=condition=complete -l app=fiod-client jobs --namespace ripsaw --timeout=300s
+  kubectl wait --for=condition=complete -l app=fiod-client-0 jobs --namespace ripsaw --timeout=300s
   sleep 30
   # ensuring the run has actually happened
   kubectl logs "$fio_pod" --namespace ripsaw | grep "Run status"
