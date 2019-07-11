633e29e1511dc8b396a89941c9b81ff9a9d1b102
Nikhil Chawla <nichawla@redhat.com>
2019-07-11 18:54:54 +0530

    Added smallfile-operator

---
+++ tests/test_smallfile.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -xeo pipefail
+
+source tests/common.sh
+
+function finish {
+  echo "Cleaning up Smallfile"
+  kubectl delete -f tests/test_crs/valid_smallfile.yaml
+  delete_operator
+}
+
+trap finish EXIT
+
+function functional_test_smallfile {
+  apply_operator
+  kubectl apply -f tests/test_crs/valid_smallfile.yaml
+  sleep 15
+  smallfile_pod=$(kubectl get pods -l app=smallfile-benchmark --namespace ripsaw -o name | cut -d/ -f2 | grep client)
+  echo smallfile_pod $smallfile_pod
+  kubectl wait --for=condition=Initialized "pods/$smallfile_pod" --namespace ripsaw --timeout=200s
+  kubectl wait --for=condition=complete -l app=smallfile-benchmark jobs --namespace ripsaw --timeout=100s
+  sleep 30
+  # ensuring the run has actually happened
+  kubectl logs "$smallfile_pod" --namespace ripsaw | grep "RUN STATUS"
+  echo "Smallfile test: Success"
+}
+
+functional_test_smallfile
