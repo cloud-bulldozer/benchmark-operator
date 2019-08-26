6b24dfeb7e091306df72feaab8f3b3351167a0a7
Ben England <bengland@redhat.com>
2019-08-26 15:17:47 -0400

    not smallfile du-uh

--- tests/test_fs_drift.sh
+++ tests/test_fs_drift.sh
@@ -17,9 +17,11 @@ function functional_test_fs_drift {
   kubectl apply -f tests/test_crs/valid_fs_drift.yaml
   sleep 15
   fsdrift_pod=$(kubectl get pods -l app=fs-drift-benchmark --namespace my-ripsaw -o name | cut -d/ -f2 | grep client)
-  echo fsdrift_pod $smallfile_pod
-  wait_for "kubectl wait --for=condition=Initialized pods/$fsdrift_pod --namespace my-ripsaw --timeout=200s" "200s" $fsdrift_pod
-  wait_for "kubectl wait --for=condition=complete -l app=smallfile-benchmark jobs --namespace my-ripsaw --timeout=100s" "100s" $fsdrift_pod
+  echo fsdrift_pod $fs_drift_pod
+  wait_for "kubectl wait --for=condition=Initialized pods/$fsdrift_pod \
+    --namespace my-ripsaw --timeout=200s" "200s" $fsdrift_pod
+  wait_for "kubectl wait --for=condition=complete -l app=fs-drift-benchmark jobs \
+    --namespace my-ripsaw --timeout=100s" "100s" $fsdrift_pod
   sleep 30
   # ensuring the run has actually happened
   kubectl logs "$fsdrift_pod" --namespace my-ripsaw | grep "RUN STATUS"
