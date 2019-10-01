619713d2a2e3363326d7155f9bcfa8aced167b05
Marko Karg <31761192+mkarg75@users.noreply.github.com>
2019-10-01 13:02:25 +0200

    Hammerdb (#208)
    * This is the squashed version of hammerdb including all the required bits and pieces to get hammerdb up and running against a MSSQL database.
    It is still lacking documentation and sophisticated benchmark runs.
    
    * initial documentation for hammerdb
    
    * syncing custom resource file with the documentation
    
    * changes to enable testing
    
    * reverting changes for snafu
    
    * reverted testing changes
    
    * fixing markdown issues

---
+++ tests/test_hammerdb.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -xeo pipefail
+
+source tests/common.sh
+
+function initdb_pod {
+	echo "Setting up a MS-SQL DB Pod"
+	kubectl create -f tests/mssql.yaml
+        mssql_pod=$(get_pod "app=mssql" 300 "sql-server")
+	kubectl wait --for=condition=Ready "pods/$mssql_pod" --namespace sql-server --timeout=300s
+}
+
+function finish {
+	echo "Cleaning up hammerdb"
+	kubectl delete -f tests/mssql.yaml 
+	kubectl delete -f tests/test_crs/valid_hammerdb.yaml
+	delete_operator
+}
+
+trap finish EXIT
+
+function functional_test_hammerdb {
+	initdb_pod
+	apply_operator
+	kubectl apply -f tests/test_crs/valid_hammerdb.yaml
+	uuid=$(get_uuid 20)
+	# Wait for the creator pod to initialize the DB
+        #DISABLED
+	#hammerdb_creator_pod=$(get_pod "app=hammerdb_creator-$uuid" 300)
+	#kubectl wait --for=condition=Initialized "pods/$hammerdb_creator_pod" --namespace my-ripsaw --timeout=100s
+	#kubectl wait --for=condition=complete -l app=hammerdb_creator-$uuid --namespace my-ripsaw jobs --timeout=600s
+	# Wait for the workload pod to run the actual workload
+	hammerdb_workload_pod=$(get_pod "app=hammerdb_workload-$uuid" 300)
+	kubectl wait --for=condition=Initialized "pods/$hammerdb_workload_pod" --namespace my-ripsaw --timeout=100s
+	kubectl wait --for=condition=complete -l app=hammerdb_workload-$uuid --namespace my-ripsaw jobs --timeout=300s
+	kubectl logs "$hammerdb_workload_pod" --namespace my-ripsaw | grep "SEQUENCE COMPLETE"
+	echo "Hammerdb test: Success"
+}
+
+functional_test_hammerdb
