10f037f729d37bc739f6927e421ba143c87c35a7
Alexander Calhoun <acalhoun@redhat.com>
2020-09-11 08:58:51 -0400

    added ci test for a valid fiod test using bsrange

--- tests/test_fiod.sh
+++ tests/test_fiod.sh
@@ -45,4 +45,5 @@ function functional_test_fio {
 
 figlet $(basename $0)
 functional_test_fio "Fio distributed" tests/test_crs/valid_fiod.yaml
+functional_test_fio "Fio distributed - bsrange" tests/test_crs/valid_fiod_bsrange.yaml
 functional_test_fio "Fio hostpath distributed" tests/test_crs/valid_fiod_hostpath.yaml
