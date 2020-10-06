486dd4a5d0c498c05375712b0f1b7405eeaa2eb5
dry923 <rzaleski@redhat.com>
2020-10-06 11:21:33 -0400

    Update test.sh to allow an http address to be passed to CI

--- test.sh
+++ test.sh
@@ -69,7 +69,7 @@ UUID=${NEW_UUID%-*}
 #Tag name
 tag_name="${NODE_NAME:-master}"
 
-sed -i "s/ES_SERVER/$ES_SERVER/g" tests/test_crs/*
+sed -i "s?ES_SERVER?$ES_SERVER?g" tests/test_crs/*
 sed -i "s/ES_PORT/$ES_PORT/g" tests/test_crs/*
 sed -i "s/sql-server/sql-server-$UUID/g" tests/mssql.yaml tests/test_crs/valid_hammerdb.yaml tests/test_hammerdb.sh
 sed -i "s/benchmarks.ripsaw.cloudbulldozer.io/benchmarks-$UUID.ripsaw.cloudbulldozer.io/g" resources/crds/ripsaw_v1alpha1_ripsaw_crd.yaml
