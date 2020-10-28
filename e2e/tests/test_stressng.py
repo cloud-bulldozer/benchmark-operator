70b7b3e3233c298f1eeb13e734fccc9cf9688d11
Keith Whitley <kwhitley@redhat.com>
2020-10-28 10:28:35 -0400

    going back to old timeouts

--- e2e/tests/test_stressng.py
+++ e2e/tests/test_stressng.py
@@ -7,5 +7,6 @@ class TestStressng(TestBase):
     workload = "stressng"
     indices = ["ripsaw-stressng-results"]
 
+    @mark.timeout(default_timeout)
     def test_stressng(self, run):
         self.run_and_check_benchmark(run, desired_running_state="Benchmark running")
