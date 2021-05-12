65ccd3f24045afdd911c745736e90a643b9b194b
ebattat <ebattat@redhat.com>
2021-05-12 15:59:34 +0300

    revert timout to 500s

--- tests/test_hammerdb.sh
+++ tests/test_hammerdb.sh
@@ -31,7 +31,7 @@ function functional_test_hammerdb {
   # Wait for the workload pod to run the actual workload
   hammerdb_workload_pod=$(get_pod "app=hammerdb_workload-$uuid" 300)
   kubectl wait --for=condition=Initialized "pods/$hammerdb_workload_pod" --namespace my-ripsaw --timeout=400s
-  kubectl wait --for=condition=complete -l app=hammerdb_workload-$uuid --namespace my-ripsaw jobs --timeout=1000s
+  kubectl wait --for=condition=complete -l app=hammerdb_workload-$uuid --namespace my-ripsaw jobs --timeout=500s
 
   index="ripsaw-hammerdb-results"
   if check_es "${long_uuid}" "${index}"
