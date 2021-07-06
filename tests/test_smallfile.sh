82c1efc232de524c0645b054dabf0fb6f5a38336
Keith Whitley <kwhitley@redhat.com>
2021-07-06 11:31:42 -0400

    adding kernel cache dropper

--- tests/test_smallfile.sh
+++ tests/test_smallfile.sh
@@ -27,8 +27,6 @@ function functional_test_smallfile {
   sed -e "s/PROMETHEUS_TOKEN/${token}/g" ${cr} | kubectl apply -f -
   long_uuid=$(get_uuid $benchmark_name)
   uuid=${long_uuid:0:8}
-  smallfile_pod=$(get_pod "app=smallfile-benchmark-$uuid" 300)
-  echo "smallfile_pod ${smallfile_pod}"
   check_benchmark_for_desired_state $benchmark_name Complete 500s
 
   indexes="ripsaw-smallfile-results ripsaw-smallfile-rsptimes"
@@ -37,9 +35,6 @@ function functional_test_smallfile {
     echo "${test_name} test: Success"
   else
     echo "Failed to find data for ${test_name} in ES"
-    for pod in ${smallfile_pod}; do
-      kubectl logs ${pod} --namespace benchmark-operator | grep "RUN STATUS"
-    done
     exit 1
   fi
   delete_benchmark $cr
