7bd9fb0ac9f1a541595d59cbe3ab813481abf7f2
Raul Sevilla <rsevilla@redhat.com>
2021-07-16 17:00:31 +0200

    Increase uperf test timeout
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- tests/test_uperf.sh
+++ tests/test_uperf.sh
@@ -31,8 +31,8 @@ function functional_test_uperf {
   uperf_server_pod=$(get_pod "type=uperf-bench-server-${uuid}" 300)
   wait_for "kubectl -n my-ripsaw wait --for=condition=Initialized -l type=uperf-bench-server-${uuid} pods --timeout=300s" "300s" $uperf_server_pod
   uperf_client_pod=$(get_pod "app=uperf-bench-client-$uuid" 900)
-  wait_for "kubectl wait -n my-ripsaw --for=condition=Initialized pods/$uperf_client_pod --timeout=500s" "500s" $uperf_client_pod
-  wait_for "kubectl wait -n my-ripsaw --for=condition=complete -l app=uperf-bench-client-$uuid jobs --timeout=500s" "500s" $uperf_client_pod
+  wait_for "kubectl wait -n my-ripsaw --for=condition=Initialized pods/$uperf_client_pod --timeout=600s" "600s" $uperf_client_pod
+  wait_for "kubectl wait -n my-ripsaw --for=condition=complete -l app=uperf-bench-client-$uuid jobs --timeout=600s" "600s" $uperf_client_pod
 
   index="ripsaw-uperf-results"
   if check_es "${long_uuid}" "${index}"
