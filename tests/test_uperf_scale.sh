18bb2d1d4123e3ba1a6dbbc19c3ab5c33eff36b0
Vishnu Challa <vchalla@redhat.com>
2022-07-12 18:23:00 -0400

    Uperf scale mode code changes (#776)
    * initial draft of standard uperf with new features
    
    * bug fix for client creation in nodeport service
    
    * bug fix for client creation conflict due to same name in metadata
    
    * initial draft for uperf-scale mode
    
    * removed uperf to avoid confusion
    
    * added index for metallb service labels
    
    * fix for client port mismatch with server and services
    
    * fixed es index issues
    
    * fixed services issue
    
    * bug fix for clusterip service ports mismatch
    
    * reverting quay repo org to cloudbulldozer
    
    * fix for service ip usecases except for nodeport
    
    * fixed ports issues for nodeport service use case
    
    * added test for uperf_scale
    
    * restoring image repo to original
    
    * removing bash eternal history
    
    * removed test configuration
    
    * fix for client start issue caused by redis variables for vm use case
    
    * renamed client_vm to client_vms for vm kind
    
    * fixing redis variables for pod use cases as well
    
    * fixed typo
    
    * resolving PR comments
    
    * removed an old comment
    
    Co-authored-by: root <root@f30-h09-000-r640.rdu2.scalelab.redhat.com>
    Co-authored-by: root <root@e16-h12-b02-fc640.rdu2.scalelab.redhat.com>
    Co-authored-by: Murali Krishnasamy <70236227+mukrishn@users.noreply.github.com>

---
+++ tests/test_uperf_scale.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -xeEo pipefail
+
+source tests/common.sh
+
+function finish {
+  if [ $? -eq 1 ] && [ $ERRORED != "true" ]
+  then
+    error
+  fi
+
+  echo "Cleaning up Uperf"
+  wait_clean
+}
+
+trap error ERR
+trap finish EXIT
+
+function functional_test_uperf {
+  token=$(oc -n openshift-monitoring sa get-token prometheus-k8s)
+  test_name=$1
+  cr=$2
+  benchmark_name=$(get_benchmark_name $cr)
+  delete_benchmark $cr
+  echo "Performing: ${test_name}"
+  sed -e "s/PROMETHEUS_TOKEN/${token}/g" ${cr} | kubectl apply -f -
+  long_uuid=$(get_uuid $benchmark_name)
+  uuid=${long_uuid:0:8}
+  check_benchmark_for_desired_state $benchmark_name Complete 1800s
+  
+
+
+  index="ripsaw-uperf-results"
+  if check_es "${long_uuid}" "${index}"
+  then
+    echo "${test_name} test: Success"
+  else
+    echo "Failed to find data for ${test_name} in ES"
+    exit 1
+  fi
+  delete_benchmark $cr
+}
+
+figlet $(basename $0)
+functional_test_uperf "Uperf scale without resources definition" tests/test_crs/valid_uperf_scale.yaml
+functional_test_uperf "Uperf scale with ServiceIP" tests/test_crs/valid_uperf_scale_serviceip.yaml
+functional_test_uperf "Uperf scale with NodePort ServiceIP" tests/test_crs/valid_uperf_scale_serviceip_nodeport.yaml
+functional_test_uperf "Uperf scale with resources definition and hostNetwork" tests/test_crs/valid_uperf_scale_resources.yaml
+functional_test_uperf "Uperf scale with networkpolicy" tests/test_crs/valid_uperf_scale_networkpolicy.yaml
